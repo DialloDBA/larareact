6c5acbc8959e6c6b3e84776f850b8e477c8b6aa9
DIALLO Abdourahamane <rahamanediallo108@gmail.com>
2024-08-31 02:58:06 -0400

    Protection des routes avec les contexts provider

--- reacto/src/components/DefaultLayout.jsx
+++ reacto/src/components/DefaultLayout.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider.jsx'
 
 function DefaultLayout() {
-  return (
-    <div>DefaultLayout</div>
-  )
+
+    const { user, token } = useStateContext();
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+    return (
+        <div>
+            <h1>Default</h1>
+            <Outlet />
+        </div>
+    )
 }
 
 export default DefaultLayout
--- reacto/src/components/GuestLayout.jsx
+++ reacto/src/components/GuestLayout.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider.jsx'
 
 function GuestLayout() {
+    const {token} = useStateContext();
+    if(token){
+        return <Navigate to="/" />
+    }
   return (
-    <div>GuestLayout</div>
+    <div>
+        <div>
+            <Outlet />
+        </div>
+    </div>
   )
 }
 
-export default GuestLayout
+export default GuestLayout  
